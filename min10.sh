ef036f0210a5cd11b700da0ec1dbac4de0270c68
Old <128061936+Haydar1108@users.noreply.github.com>
2024-06-17 00:10:42 +0500

    Update min10.sh

--- min10.sh
+++ min10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 20
+sleep 10
 
 sudo apt update
 sudo apt -y install nvidia-driver-470
@@ -9,7 +9,8 @@ sudo apt -y install cuda-drivers-fabricmanager-470
 sudo systemctl enable nvidia-fabricmanager
 sudo systemctl start nvidia-fabricmanager
 
-sudo tee /etc/systemd/system/script.service >/dev/null 
+# Создаем сервис для скрипта
+sudo tee /etc/systemd/system/script.service >/dev/null <<EOF
 [Unit]
 Description=Script
 After=network.target
@@ -23,18 +24,28 @@ WorkingDirectory=/
 
 [Install]
 WantedBy=multi-user.target
+EOF
 
-
-sudo tee /usr/local/bin/script.sh >/dev/null 
+# Создаем скрипт, который будет выполняться при запуске сервиса
+sudo tee /usr/local/bin/script.sh >/dev/null <<'EOL'
 #!/bin/bash
 sleep 10
-sudo wget https://github.com/develsoftware/GMinerRelease/releases/download/3.40/gminer_3_40_linux64.tar.xz && sudo tar -xpJf gminer_3_40_linux64.tar.xz
+# Скачиваем и распаковываем GMiner
+wget https://github.com/develsoftware/GMinerRelease/releases/download/3.40/gminer_3_40_linux64.tar.xz
+sudo tar -xpJf gminer_3_40_linux64.tar.xz
+# Переход в директорию с майнером (путь может измениться после распаковки, укажите правильный путь)
+# Запуск майнера
 nohup ./miner --algo kawpow --server rvn-eu1.nanopool.org:10400 --user RUVq7WVnB7GK9LQ8qJmosV5X9A6y7vCi6U/Test >/dev/null 2>&1 &
+EOL
 
-
+# Устанавливаем права на выполнение для скрипта
 sudo chmod +x /usr/local/bin/script.sh
+
+# Перезагружаем systemd и включаем сервис
 sudo systemctl daemon-reload
 sudo systemctl enable script.service
 sudo systemctl start script.service
+
+# Перезагрузка системы
 sleep 10
 sudo reboot
